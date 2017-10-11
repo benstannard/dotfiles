85fb48341d6952a772d79c9789196ad0b32933f8
Ben Stannard <ben.stannard@gmail.com>
2017-10-11 14:40:04 -0700

    add gitconfig

--- install.bash
+++ install.bash
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo "Updating dotfiles"
+cd $HOME
 rm .bashrc
+mkdir .emacs.d
 ln -s dotfiles/bashrc ~/.bashrc
 ln -s dotfiles/init.el ~/.emacs.d/init.el
 ln -s dotfiles/vimrc ~/.vimrc
 ln -s dotfiles/tmux.conf ~/.tmux.conf
+ln -s dotfiles/gitconfig ~/.gitconfig
 source ~/.bashrc
 echo "Finished."
