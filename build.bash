9906502b218d8471c58ee5340240e37eabfba762
Ben Stannard <ben.stannard@gmail.com>
2018-04-08 11:44:56 -0700

    added screenrc

--- build.bash
+++ build.bash
@@ -5,13 +5,14 @@ cd $HOME
 
 # remove dotfiles
 rm .bashrc
+rm .screenrc
 mkdir .emacs.d
 
 # create symlinks
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/init.el ~/.emacs.d/init.el
 ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/screenrc ~/.screenrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 
 # refresh & exit
