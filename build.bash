3792cdfd801be532ef8b96628d4ce1fc98407957
Ben Stannard <ben.stannard@gmail.com>
2018-08-11 11:42:15 -0700

    removed tmux settings, using screen

--- build.bash
+++ build.bash
@@ -11,7 +11,6 @@ mkdir .emacs.d
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/init.el ~/.emacs.d/init.el
 ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 
 # refresh & exit
